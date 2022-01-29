08110fc93a5ffdaea949388f7a35c219e325935f
doct3rjohn <ada7700ada@gmail.com>
2022-01-29 23:25:24 +0800

    minor_changes

--- src/budu.py
+++ src/budu.py
@@ -1,4 +1,4 @@
-import sys, json
+import sys, json, os
 
 class c:
     BMAGENTA = '\033[95m'
@@ -27,21 +27,35 @@ def help_me():
     print("")
     print(f" {c.BBLUE}###################################################")
     print(f" #                                                 #{c.RESET}")
-    print(f" {c.BBLUE}#{c.RESET} {c.BOLD}{c.BBLACK}{c.BYELLOW}Example: petai.py -s 1 -lh 10.10.10.10 -lp 1000{c.RESET} {c.BBLUE}#{c.RESET}")
+    print(f" {c.BBLUE}#{c.RESET} {c.BOLD}{c.BBLACK}{c.BYELLOW}Example: petai.py -id 1 -i 10.10.10.10 -p 1000{c.RESET} {c.BBLUE}#{c.RESET}")
     print(f" {c.BBLUE}#                                                 #")
     print(f" ###################################################{c.RESET}\n")
-    print(f" {c.BGREEN}{c.UNDERLINE}Usage:{c.RESET} petai.py [{c.BGREEN}--help{c.RESET}]  [-s [{c.BGREEN}REVERSE SHELL ID{c.RESET}]]  [-lh [{c.BGREEN}LHOST{c.RESET}]]  [-lp [{c.BGREEN}LPORT{c.RESET}]]")
+    print(f" {c.BGREEN}{c.UNDERLINE}Usage:{c.RESET} petai.py [{c.BGREEN}--help{c.RESET}]  [-id [{c.BGREEN}REVERSE SHELL ID{c.RESET}]]  [-i [{c.BGREEN}LHOST{c.RESET}]]  [-p [{c.BGREEN}LPORT{c.RESET}]]")
     print(f" \n\n {c.UNDERLINE}{c.BOLD}{c.BBLACK}{c.BYELLOW}OPTIONS:{c.RESET}")
-    print(" -s   / --shell <reverse_shell_id>     :   Reverse Shell ID")
-    print(" -lh  / --lhost <ip_address>/LHOST     :   Ip Address")
-    print(" -lp  / --lport <listening_port/LPORT> :   Port")
+    print(" -id / --id <reverse_shell_id>        :   Reverse Shell ID")
+    print(" -i  / --lhost <ip_address>/LHOST     :   Ip Address")
+    print(" -p  / --lport <listening_port/LPORT> :   Port")
     print(" ")
     print(f" {c.UNDERLINE}{c.BOLD}{c.BBLACK}{c.BYELLOW}WHY NOT OPTIONS:{c.RESET}")
     print(" --help:      show help")
     print(" --list:      list all reverse shell [ID]")
 
 def index_err():
-    print(f" {c.BGREEN}{c.UNDERLINE}Usage:{c.RESET} petai.py [{c.BGREEN}--help{c.RESET}]  [-s [{c.BGREEN}REVERSE SHELL ID{c.RESET}]]  [-lh [{c.BGREEN}LHOST{c.RESET}]]  [-lp [{c.BGREEN}LPORT{c.RESET}]]\n\n")
-    print(f" {c.BOLD}{c.BBLACK}{c.BYELLOW}Example: petai.py -s 1 -lh 10.10.10.10 -lp 1000{c.RESET}")
-    print(f" {c.BOLD}{c.BBLACK}{c.BYELLOW}Example: petai.py --shell 1 --lhost 10.10.10.10 --lport 1000{c.RESET}")
+    print(f" {c.BGREEN}{c.UNDERLINE}Usage:{c.RESET} petai.py [{c.BGREEN}--help{c.RESET}]  [-id [{c.BGREEN}REVERSE SHELL ID{c.RESET}]]  [-i [{c.BGREEN}LHOST{c.RESET}]]  [-p [{c.BGREEN}LPORT{c.RESET}]]\n\n")
+    print(f" {c.BOLD}{c.BBLACK}{c.BYELLOW}Example: petai.py -id 1 -i 10.10.10.10 -p 1000{c.RESET}")
+    print(f" {c.BOLD}{c.BBLACK}{c.BYELLOW}Example: petai.py --id 1 --lhost 10.10.10.10 --lport 1000{c.RESET}")
     print(f" {c.BYELLOW}Error{c.RESET}: Sorry, Try This '{c.BGREEN}--help{c.RESET}'")
+
+def rvshell(id, lhost, lport):
+    with open("src/jering.json", 'r') as f:
+        data = json.loads(f.read())
+        for i in data["reverse_shell_list"]:
+            if i['id'] == id:
+                payload = i['shell']
+                xp = payload.replace("lhost", lhost)
+                vista = xp.replace("lport", lport)
+                print(f"\n {c.BOLD}{c.BGREEN}The Command:{c.RESET} {vista}")
+                print("---------------------------------------------------")
+                print(f" [{c.BGREEN}X{c.RESET}] {c.BYELLOW}stARt LIsTeNing...{c.RESET}")
+                os.system(f"nc -lnvp {lport}")
+                break
