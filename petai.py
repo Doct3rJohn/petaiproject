2b5247c4b2ee27e1bec46c5b5c9b2971b49050b0
doct3rjohn <ada7700ada@gmail.com>
2022-01-31 01:57:13 +0800

    v0.0.3.2

--- petai.py
+++ petai.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 #Author     : Doct3rJohn @_shafiqaiman_
-<<<<<<< HEAD
 #Created    : 16:03:2021
 #Update     : 31:1:2022
 #Version    : v0.0.3.2
-=======
-#Date       : 16:03:2021
-#Update     : 30:01:2022
-#Version    : v0.0.3   
->>>>>>> 80616311cc7b5ea56ce2e0fdbe51e648857c35af
 
 from src.main import main
 
